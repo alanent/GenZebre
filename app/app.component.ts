635aa68826c099f4c3046e185f2acc643dc68381
alanent <33031877+alanent@users.noreply.github.com>
2021-09-29 14:07:30 +0200

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -19,9 +19,8 @@ export class AppComponent {
 
   // array is selectedQuestions
   selectAll(select: MatSelect, values, array) {
-    select.value = values;
-    array = values;
-    console.log(this.selectedQuestions); // selectedQuestions is still undefined
+  this.selectedQuestions = this.questions;
+  console.log(this.selectedQuestions); // selectedQuestions is still undefined
   }
 
   deselectAll(select: MatSelect) {
